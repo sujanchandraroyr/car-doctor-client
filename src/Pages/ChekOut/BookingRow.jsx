88d92682835c3de9b520e577141df023cdaef8e2
sujanchandrar <roys33664@gmail.com>
2024-05-17 06:38:46 +0600

    booking service update confirm added

--- src/Pages/ChekOut/BookingRow.jsx
+++ src/Pages/ChekOut/BookingRow.jsx
@@ -1,46 +1,58 @@
+import PropTypes from 'prop-types'
 
-const BookingRow = ({booking}) => {
-    const {service, price, img, email, date, coustomerName} = booking;
+
+const BookingRow = ({ booking, handleDeleteService, handleBookingConfirm }) => {
+    
+    const { service, price, img, email, date, coustomerName, _id, status } = booking;
+    
     return (
         <div>
-            <table className="table">
-               
 
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className=" w-24 rounded h-24">
-                                        <img src={img} alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">{service}</div>
-                                    <div className="text-sm opacity-50">{}</div>
-                                </div>
+
+            <table className="table w-full">
+            <tr>
+                <th>
+                <button onClick={() => handleDeleteService(_id)} className="btn btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                </th>
+                <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                            <div className=" w-24 rounded h-24">
+                                <img src={img} alt="Avatar Tailwind CSS Component" />
                             </div>
-                        </td>
-                        <td>{coustomerName}</td>
-                        <td>
-                            <p>{email}</p>
-                        </td>
-                        <td>{date}</td>
-                        <td>{price}</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
+                        </div>
+                        <div>
+                            <div className="font-bold">{service}</div>
+                            <div className="text-sm opacity-50">{ }</div>
+                        </div>
+                    </div>
+                </td>
+                <td>{coustomerName}</td>
+                <td>
+                    <p>{email}</p>
+                </td>
+                <td>{date}</td>
+                <td>{price}</td>
+                <th>
+                {status === 'confirm' ? <p className='text-primary font-bold'>Confirm</p> :
+                    <button onClick={() => handleBookingConfirm(_id)} className="btn ">
+                    Pelease Confirm
+                </button>}
+                </th>
+            </tr>
+        </table>
+</div>
 
 
-                
-            </table>
-        </div>
+
     );
 };
 
+BookingRow.propTypes = {
+    booking: PropTypes.object,
+    handleDeleteService: PropTypes.func,
+    handleBookingConfirm: PropTypes.func
+}
 export default BookingRow;
--- src/Pages/ChekOut/Bookings.jsx
+++ src/Pages/ChekOut/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import toast from "react-hot-toast";
 
 
 const Bookings = () => {
@@ -12,31 +13,75 @@ const Bookings = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setBookings(data))
-    },[])
+    },[url])
+
+    const handleDeleteService = (id) => {
+
+      const procced = confirm('Are you sure delete service')
+      
+
+      if(procced){
+          fetch(`http://localhost:5000/bookings/${id}`, {
+          method: 'DELETE',
+          
+      })
+      .then(res => res.json())
+      .then(data => {
+         
+          if(data.deletedCount > 0){
+              toast.success('Delete Service Successfully ')
+              const remaing = bookings.filter(booking => booking._id !== id)
+              
+              setBookings(remaing)
+          }
+      })
+      }
+
+  }
+
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({status: 'confirm'})
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.modifiedCount > 0){
+            const remaining = bookings.filter(booking => booking._id !== id);
+            const update = bookings.find(booking => booking._id === id);
+            update.status = 'confirm'
+            const newBookings = [update, ...remaining]
+            setBookings(newBookings)
+        }
+    })
+  }
+
     return (
         <div>
             <div className="overflow-x-auto">
-  <table className="table">
+  <table className="table w-full">
     
    
       <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
         <th></th>
-        <th>Name</th>
+        <th></th>
+        <th>Service</th>
+        <th>Coustomer Name</th>
         <th>Email</th>
         <th>Date</th>
         <th>Price</th>
+        <th></th>
       </tr>
     
   </table>
   
 </div>
 {
-        bookings.map((booking) => <BookingRow key={booking._id} booking={booking}></BookingRow>)
+        bookings.map((booking) => <BookingRow key={booking._id} booking={booking} handleDeleteService={handleDeleteService} handleBookingConfirm={handleBookingConfirm}></BookingRow>)
     }
         </div>
     );
